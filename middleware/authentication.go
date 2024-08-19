a18b0f01c58640d1a3234666d35e3cb0cc82240f
ragnarrlaw <carmichealclair@yahoo.com>
2024-08-19 08:50:25 +0530

    update the project structure (removed and moved files to change the structure)

--- middleware/authentication.go
+++
@@ -1,6 +0,0 @@
-package middleware
-
-/*
-   Implementing the JWT token authenticaton to check whether the user is logged in or not.
-   Can handle the registration and login tasks here as well.
-*/
--- middleware/authorization.go
+++
@@ -1,6 +0,0 @@
-package middleware
-
-/*
-   Check for user permissions admin, user and so.
-   Can handle the public route cases as well.
-*/
