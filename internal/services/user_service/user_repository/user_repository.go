c00e299d5dea264f5db884ad481a08c01bc4e8f1
ragnarrlaw <carmichealclair@yahoo.com>
2024-08-19 17:44:42 +0530

    complete basic crud functions in user service and user repository

--- internal/services/user_service/user_repository/user_repository.go
+++ internal/services/user_service/user_repository/user_repository.go
@@ -8,11 +8,11 @@ import (
 )
 
 type UserRepository interface {
-	Add(context.Context, *types.User) (*types.User, error)
+	Add(context.Context, *types.UserSignUpPayload) (*types.User, error)
 	GetById(context.Context, string) (*types.User, error)
 	GetAll(context.Context) (*[]types.User, error)
 	Remove(context.Context, []string) error
-	Update(context.Context, *types.User) (*types.User, error)
+	Update(context.Context, string, *types.UserUpdatePayload) (*types.User, error)
 }
 
 type UserRepo struct {
@@ -23,16 +23,55 @@ func NewUserRepo(storage *database.Storage) UserRepository {
 	return &UserRepo{storage: storage}
 }
 
-func (repo *UserRepo) Add(ctx context.Context, u *types.User) (*types.User, error) {
-	return nil, nil
-}
-
-func (repo *UserRepo) GetById(ctx context.Context, id string) (*types.User, error) {
-	query := "SELECT id, name, email FROM users WHERE id = $1"
+func (repo *UserRepo) Add(ctx context.Context, u *types.UserSignUpPayload) (*types.User, error) {
+	query := "INSERT INTO users (username, first_name, last_name, email, contact_number, password_digest) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, username, first_name, last_name, email, contact_number, password_digest"
 	var user types.User
 	err := repo.storage.GetRow(
 		ctx,
-		query, []string{id}, &user.ID, &user.Name, &user.Email)
+		query,
+		[]interface{}{
+			u.Username,
+			u.FirstName,
+			u.LastName,
+			u.Email,
+			u.ContactNumber,
+			u.Password,
+		},
+		[]interface{}{
+			&user.Id,
+			&user.Username,
+			&user.FirstName,
+			&user.LastName,
+			&user.Email,
+			&user.ContactNumber,
+			&user.Password,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (repo *UserRepo) GetById(ctx context.Context, id string) (*types.User, error) {
+	query := "SELECT id, username, first_name, last_name, email, contact_number, password_digest FROM users WHERE id = $1"
+	var user types.User
+	err := repo.storage.GetRow(
+		ctx,
+		query,
+		[]interface{}{
+			id,
+		},
+		[]interface{}{
+			&user.Id,
+			&user.Username,
+			&user.FirstName,
+			&user.LastName,
+			&user.Email,
+			&user.ContactNumber,
+			&user.Password,
+		},
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -40,13 +79,64 @@ func (repo *UserRepo) GetById(ctx context.Context, id string) (*types.User, erro
 }
 
 func (repo *UserRepo) GetAll(ctx context.Context) (*[]types.User, error) {
-	return nil, nil
+	query := "SELECT id, username, first_name, last_name, email, contact_number, password_digest FROM users"
+	rows, err := repo.storage.GetRows(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	var users []types.User
+	for _, row := range rows {
+		user := types.User{
+			Id:            row["id"].(string),
+			Username:      row["username"].(string),
+			FirstName:     row["first_name"].(string),
+			LastName:      row["last_name"].(string),
+			Email:         row["email"].(string),
+			ContactNumber: row["contact_number"].(string),
+			Password:      row["password_digest"].(string),
+		}
+		users = append(users, user)
+	}
+	return &users, nil
 }
 
 func (repo *UserRepo) Remove(ctx context.Context, ids []string) error {
+	query := "DELETE FROM users WHERE id = ANY($1)"
+	err := repo.storage.Exec(ctx, query, ids)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (repo *UserRepo) Update(ctx context.Context, u *types.User) (*types.User, error) {
-	return nil, nil
+func (repo *UserRepo) Update(ctx context.Context, id string, u *types.UserUpdatePayload) (*types.User, error) {
+	query := "UPDATE users SET username = $1, first_name = $2, last_name = $3, email = $4, contact_number = $5, password_digest = $6 WHERE id = $7 RETURNING id, username, first_name, last_name, email, contact_number, password_digest"
+	var user types.User
+	err := repo.storage.GetRow(
+		ctx,
+		query,
+		[]interface{}{
+			u.Username,
+			u.FirstName,
+			u.LastName,
+			u.Email,
+			u.ContactNumber,
+			u.Password,
+			id,
+		},
+		[]interface{}{
+			&user.Id,
+			&user.Username,
+			&user.FirstName,
+			&user.LastName,
+			&user.Email,
+			&user.ContactNumber,
+			&user.Password,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
--- internal/services/user_service/user_service.go
+++ internal/services/user_service/user_service.go
@@ -1,23 +1,63 @@
 package userservice
 
 import (
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 
-	"github.com/raganrrlaw/server/internal/services/user_service/user_repository"
+	userrepository "github.com/raganrrlaw/server/internal/services/user_service/user_repository"
+	"github.com/raganrrlaw/server/internal/types"
+	"github.com/raganrrlaw/server/internal/utils"
 )
 
 type UserService struct {
-  userRepository *userrepository.UserRepository
+	userRepository userrepository.UserRepository
 }
 
-func NewUserService(repo *userrepository.UserRepository) *UserService {
-  return &UserService{
-    userRepository: repo,
-  }
+func NewUserService(repo userrepository.UserRepository) *UserService {
+	return &UserService{
+		userRepository: repo,
+	}
 }
 
-func (us *UserService) AddUserHandler(w http.ResponseWriter, r *http.Request) {}
+/** Used mainly for testing purposes */
+func (us *UserService) AddUserHandler(w http.ResponseWriter, r *http.Request) {
+	payload := &types.UserSignUpPayload{}
+	err := json.NewDecoder(r.Body).Decode(payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
+	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+	defer cancel()
+
+	/** TODO: PAYLOAD VALIDATION */
+	/** TODO: GENERIC RESPONSE ERROR STRUCTURE ORGANIZATION */
+
+	if digest, err := utils.HashPassword(payload.Password); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		payload.Password = digest
+		user, err := us.userRepository.Add(ctx, payload)
+		log.Printf(">>>> Created User: %s\n", user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		} else {
+			if b, err := json.Marshal(user); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			} else {
+				w.Write(b)
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusCreated)
+			}
+		}
+	}
+}
+
+/** Used mainly for testing purposes */
 func (us *UserService) RemoveUserHandler(w http.ResponseWriter, r *http.Request) {}
 
 func (us *UserService) UpdateUserHandler(w http.ResponseWriter, r *http.Request) {}
