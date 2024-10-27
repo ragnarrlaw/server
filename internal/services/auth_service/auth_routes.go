60141ad501b85af64375ba0adaa28b9af04f8628
ragnarrlaw <carmichealclair@yahoo.com>
2024-10-27 12:57:31 +0530

    add registration routes for users, add login routes for users, add logout route for users and add token refresh route

--- internal/services/auth_service/auth_routes.go
+++ internal/services/auth_service/auth_routes.go
@@ -1,9 +1,22 @@
 package authservice
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/raganrrlaw/server/internal/middleware"
+)
 
 func (as *AuthService) RegisterRoutes(router *http.ServeMux) {
-  router.HandleFunc("POST /login", as.LoginHandler)
-  router.HandleFunc("POST /register", as.SignupHandler)
-  router.HandleFunc("POST /logout", as.LogoutHandler)
+	router.HandleFunc("POST /login", as.LoginHandler)
+	router.HandleFunc("POST /register", as.SignUpHandler)
+	router.Handle("POST /logout",
+		middleware.ValidateAccessTokens(
+			middleware.ValidateRefreshTokens(
+				http.HandlerFunc(as.LogoutHandler),
+			),
+		),
+	)
+	router.Handle("POST /refresh", middleware.ValidateRefreshTokens(
+		http.HandlerFunc(as.RefreshAccessTokenHandler)),
+	)
 }
