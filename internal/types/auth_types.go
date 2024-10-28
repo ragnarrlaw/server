4d40b53960efa0d74f556deb127128d09ccd6c2a
ragnarrlaw <carmichealclair@yahoo.com>
2024-10-28 07:37:44 +0530

    extract common types to individual files

---
+++ internal/types/auth_types.go
@@ -0,0 +1,46 @@
+package types
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+// contextKey is a type used for context keys to avoid conflicts
+type ContextKey string
+
+const (
+	UserIDKey       ContextKey = "user_id"
+	RefreshTokenKey ContextKey = "refresh_token"
+	AccessTokenKey  ContextKey = "access_token"
+)
+
+type Token struct {
+	Type        string `json:"type"`
+	AccessToken string `json:"access_token"`
+}
+
+func (t *Token) String() string {
+	return fmt.Sprintf("Token: { Type: %s, AccessToken: %s }", t.Type, t.AccessToken)
+}
+
+type TokenClaims struct {
+	UserId string `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func (tc *TokenClaims) String() string {
+	return fmt.Sprintf("TokenClaims: { UserId: %s }", tc.UserId)
+}
+
+type AuthToken struct {
+	Id     uuid.UUID `json:"id"`
+	UserId string `json:"user_id"`
+	Token  string `json:"token"`
+}
+
+func (at *AuthToken) String() string {
+	return fmt.Sprintf("AuthToken: { Id: %s, UserId: %s, Token: %s }", at.Id, at.UserId, at.Token)
+}
+
