42c2157e6c0906b765637e060ff29a226a082cfc
ragnarrlaw <carmichealclair@yahoo.com>
2024-10-31 09:46:30 +0530

    add store related routes

---
+++ internal/services/store_service/store_routes.go
@@ -0,0 +1,11 @@
+package storeservice
+
+import "net/http"
+
+func (ss *StoreService) RegisterRoutes(router *http.ServeMux) {
+  router.HandleFunc("GET /store", ss.GetAllStoresHandler)
+  router.HandleFunc("GET /store/{id}", ss.GetStoreHandler)
+  router.HandleFunc("POST /store", ss.AddStoreHandler)
+  router.HandleFunc("PATCH /store/{id}", ss.UpdateStoreHandler)
+  router.HandleFunc("DELETE /store/{id}", ss.RemoveStoreHandler)
+}
