106a91d3023fb6d7295a55cb3515195b95339132
ragnarrlaw <carmichealclair@yahoo.com>
2024-10-31 09:47:02 +0530

    add basic crud functionalities for the store

---
+++ internal/services/store_service/store_service.go
@@ -0,0 +1,26 @@
+package storeservice
+
+import (
+	"net/http"
+
+	storerepository "github.com/raganrrlaw/server/internal/services/store_service/store_repository"
+)
+
+type StoreService struct {
+	storeRepository storerepository.StoreRepository
+}
+
+func NewStoreService(storeRepository storerepository.StoreRepository) *StoreService {
+	return &StoreService{storeRepository: storeRepository}
+}
+
+func (ss *StoreService) GetAllStoresHandler (w http.ResponseWriter, r *http.Request) {}
+
+func (ss *StoreService) GetStoreHandler (w http.ResponseWriter, r *http.Request) {}
+
+func (ss *StoreService) AddStoreHandler (w http.ResponseWriter, r *http.Request) {}
+
+func (ss *StoreService) UpdateStoreHandler (w http.ResponseWriter, r *http.Request) {}
+
+func (ss *StoreService) RemoveStoreHandler (w http.ResponseWriter, r *http.Request) {}
+
