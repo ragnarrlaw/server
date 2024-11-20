c5f17827ca756e5e17c502cb29c5e93c59c6615e
ragnarrlaw <carmichealclair@yahoo.com>
2024-11-20 15:33:57 +0530

    update the auth repo to handler user-side and store-side login/signup cases

--- internal/services/auth_service/auth_repository/auth_repository.go
+++ internal/services/auth_service/auth_repository/auth_repository.go
@@ -2,11 +2,8 @@ package authrepository
 
 import (
 	"context"
-	"fmt"
-	"log"
 
-	uuid "github.com/google/uuid"
-	database "github.com/raganrrlaw/server/internal/db"
+	database "github.com/raganrrlaw/server/db/database"
 	"github.com/raganrrlaw/server/internal/types"
 )
 
@@ -31,80 +28,102 @@ func NewAuthRepo(storage *database.Storage) AuthRepository {
 
 /** GetById fetches a token by id */
 func (ar *AuthRepo) GetById(ctx context.Context, id string) (*types.AuthToken, error) {
-	query := "SELECT id, user_id, token FROM auth_token WHERE id = $1"
+	query := "SELECT id, user_id, role, token FROM auth_token WHERE id = $1"
 	var authToken types.AuthToken
-	if err := ar.storage.GetRow(
+	row := ar.storage.Pool.QueryRow(
 		ctx,
 		query,
-		[]interface{}{id},
-		[]interface{}{
-			&authToken.Id,
-			&authToken.UserId,
-			&authToken.Token,
-		},
+		id,
+	)
+	if err := row.Scan(
+		&authToken.Id,
+		&authToken.UserId,
+		&authToken.Role,
+		&authToken.Token,
 	); err != nil {
-		log.Printf("Error fetching auth token by id: %v", err)
-		return nil, fmt.Errorf("failed to fetch auth token by id: %w", err)
+		return nil, err
+	} else {
+		return &authToken, nil
 	}
-	return &authToken, nil
 }
 
 /** AddToken adds a new token to the database */
 func (ar *AuthRepo) AddToken(ctx context.Context, authToken *types.AuthToken) (*types.AuthToken, error) {
-	query := "INSERT INTO auth_token (user_id, token) VALUES ($1, $2) RETURNING id"
-	if err := ar.storage.GetRow(
+	query := "INSERT INTO auth_token (user_id, role, token) VALUES ($1, $2, $3) RETURNING id"
+
+	var token types.AuthToken
+	row := ar.storage.Pool.QueryRow(
 		ctx,
 		query,
-		[]interface{}{
-			authToken.UserId,
-			authToken.Token,
-		},
-		[]interface{}{
-			&authToken.Id,
-		},
+		authToken.UserId,
+		authToken.Role,
+		authToken.Token,
+	)
+	if err := row.Scan(
+		&authToken.Id,
+		&authToken.UserId,
+		&authToken.Role,
+		&authToken.Token,
 	); err != nil {
-		log.Printf("Error adding auth token: %v", err)
-		return nil, fmt.Errorf("failed to add auth token: %w", err)
+		return nil, err
+	} else {
+		return &token, nil
 	}
-	return authToken, nil
 }
 
 /** GetAssociatedTokens fetches all tokens associated with a user */
 func (ar *AuthRepo) GetAssociatedTokens(ctx context.Context, userId string) (*[]types.AuthToken, error) {
-	query := "SELECT id, user_id, token FROM auth_token WHERE user_id = $1"
-	rows, err := ar.storage.GetRows(ctx, query, userId)
-	if err != nil {
-		log.Printf("Error fetching associated tokens: %v", err)
-		return nil, fmt.Errorf("failed to fetch associated tokens: %w", err)
-	}
-	var authTokens []types.AuthToken
-	for _, row := range rows {
-		authToken := types.AuthToken{
-			Id:     row["id"].(uuid.UUID),
-			UserId: row["user_id"].(string),
-			Token:  row["token"].(string),
+	query := "SELECT id, user_id, role, token FROM auth_token WHERE user_id = $1"
+
+	if rows, err := ar.storage.Pool.Query(
+		ctx,
+		query,
+		userId,
+	); err != nil {
+		return nil, err
+	} else {
+		var authTokens []types.AuthToken
+		for rows.Next() {
+			var authToken types.AuthToken
+			if err := rows.Scan(
+				&authToken.Id,
+				&authToken.UserId,
+				&authToken.Role,
+				&authToken.Token,
+			); err != nil {
+				return nil, err
+			}
+			authTokens = append(authTokens, authToken)
 		}
-		authTokens = append(authTokens, authToken)
+		return &authTokens, nil
 	}
-	return &authTokens, nil
 }
 
 /** RemoveToken removes a single token associated with a token id  */
 func (ar *AuthRepo) RemoveToken(ctx context.Context, tokenIds []string) error {
 	query := "DELETE FROM auth_token WHERE id = ANY($1)"
-	if err := ar.storage.Exec(ctx, query, tokenIds); err != nil {
-		log.Printf("Error removing token: %v", err)
-		return fmt.Errorf("failed to remove token: %w", err)
+
+	if _, err := ar.storage.Pool.Exec(
+		ctx,
+		query,
+		tokenIds,
+	); err != nil {
+		return err
+	} else {
+		return nil
 	}
-	return nil
 }
 
 /** RemoveTokensOfUser removes all tokens associated with a user */
 func (ar *AuthRepo) RemoveTokensOfUser(ctx context.Context, userId string) error {
 	query := "DELETE FROM auth_token WHERE user_id = $1"
-	if err := ar.storage.Exec(ctx, query, userId); err != nil {
-		log.Printf("Error removing tokens of user: %v", err)
-		return fmt.Errorf("failed to remove tokens of user: %w", err)
+	if _, err := ar.storage.Pool.Exec(
+		ctx,
+		query,
+		userId,
+	); err != nil {
+		return err
+	} else {
+		return nil
 	}
-	return nil
 }
