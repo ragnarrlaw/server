e6ea16d905f9658f84ddd5ef692a3ddc2d863f72
ragnarrlaw <carmichealclair@yahoo.com>
2024-11-20 15:35:47 +0530

    update imports after internal package file changes

--- internal/services/public_service/index_service.go
+++ internal/services/public_service/index_service.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/raganrrlaw/server/internal/config"
+	"github.com/raganrrlaw/server/config"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
