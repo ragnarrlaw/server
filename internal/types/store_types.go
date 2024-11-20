1b90d69d9bb79bc2cbd032c629f991e7d0938012
ragnarrlaw <carmichealclair@yahoo.com>
2024-11-20 15:43:31 +0530

    update store types reflect the schema changes in the store table

--- internal/types/store_types.go
+++ internal/types/store_types.go
@@ -1,34 +1,55 @@
 package types
 
-import uuid "github.com/google/uuid"
+import (
+	"fmt"
+
+	uuid "github.com/google/uuid"
+)
 
 type StoreSignUpPayload struct {
-	StoreName     string `json:"store_name"`
+	StoreName     string `json:"storeName"`
+	Username      string `json:"username"`
 	Address       string `json:"address"`
-	Email         string `json:"store_email"`
-	ContactNumber string `json:"contact_number"` // this should be a list of numbers
+	Email         string `json:"storeEmail"`
+	ContactNumber string `json:"contactNumber"`
+	WebURL        string `json:"storeWebUrl"`
 	Password      string `json:"password"`
 }
 
 type StoreLoginPayload struct {
-	StoreUsername string `json:"store_username"`
-	Password      string `json:"password"`
+	Username string     `json:"username"`
+	Password string     `json:"password"`
+	Role     EntityType `json:"role"`
 }
 
 type StoreUpdatePayload struct {
-	StoreUsername      string `json:"store_username" db:"store_username"`
-	StoreName          string `json:"store_name" db:"store_name"`
-	StoreEmail         string `json:"store_email" db:"store_email"`
-	StoreContactNumber string `json:"store_contact_number" db:"store_contact_number"`
+	StoreUsername      string `json:"storeUsername" db:"store_username"`
+	StoreName          string `json:"storeName" db:"store_name"`
+	StoreEmail         string `json:"storeEmail" db:"store_email"`
+	StoreWebURL        string `json:"storeWebUrl" db:"store_web_url"`
+	StoreContactNumber string `json:"storeContactNumber" db:"store_contact_number"`
 }
 
 type Store struct {
 	Id                 uuid.UUID `json:"id" db:"id"`
-	StoreUsername      string    `json:"store_username" db:"store_username"`
-	StoreName          string    `json:"store_name" db:"store_name"`
-	StoreAddress       string    `json:"store_address" db:"store_address"`
-	StoreEmail         string    `json:"store_email" db:"store_email"`
-	StoreContactNumber string    `json:"store_contact_number" db:"store_contact_number"`
-	Password           string    `json:"password_digest" db:"password_digest"`
-	// StoreLocation      string    `json:"store_location" db:"store_location"`
+	StoreUsername      string    `json:"storeUsername" db:"store_username"`
+	StoreName          string    `json:"storeName" db:"store_name"`
+	StoreAddress       string    `json:"storeAddress" db:"store_address"`
+	StoreEmail         string    `json:"storeEmail" db:"store_email"`
+	StoreContactNumber string    `json:"storeContactNumber" db:"store_contact_number"`
+	StoreWebURL        string    `json:"storeWebUrl" db:"store_web_url"`
+	Password           string    `db:"password_digest"`
+	StoreLocation      GeoPoint  `json:"storeLocation" db:"store_location"`
+}
+
+func (s *Store) String() string {
+	return fmt.Sprintf("Store: { Id: %s, StoreUsername: %s, StoreName: %s, StoreAddress: %s, StoreEmail: %s, StoreContactNumber: %s, StoreWebUrl: %s, StoreLocation: {latitude: %v, longitude: %v} }", s.Id, s.StoreUsername, s.StoreName, s.StoreAddress, s.StoreEmail, s.StoreContactNumber, s.StoreWebURL, s.StoreLocation.Latitude, s.StoreLocation.Longitude)
+}
+
+func (s *Store) GetId() string {
+	return s.Id.String()
+}
+
+func (s *Store) GetUsername() string {
+	return s.StoreUsername
 }
