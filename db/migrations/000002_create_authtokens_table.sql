145fc8f6b0433dedd180dfbbc5439aa9d3c02ab9
ragnarrlaw <carmichealclair@yahoo.com>
2024-11-21 21:14:24 +0530

    update token length varchar value to fit the new token length

--- db/migrations/000002_create_authtokens_table.sql
+++ db/migrations/000002_create_authtokens_table.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION IF NOT EXISTS "citext";
 CREATE TABLE auth_token (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL,
-    token VARCHAR(255) NOT NULL,
+    token VARCHAR(512) NOT NULL,
     role VARCHAR(4) NOT NULL CHECK (role IN ('user', 'store')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
