a6d9a144c04ae8764e8204106a45b69569cb700f
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-12 20:53:01 +0530

    remove role from store_auth_token relation

--- db/migrations/000004_create_store_auth_tokens_table.sql
+++ db/migrations/000004_create_store_auth_tokens_table.sql
@@ -6,7 +6,6 @@ CREATE TABLE store_auth_token (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     store_id UUID NOT NULL,
     token VARCHAR(512) NOT NULL,
-    role VARCHAR(5) NOT NULL CHECK (role IN ('store')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (store_id) REFERENCES store(id) ON DELETE CASCADE
