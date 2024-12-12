727e061b361f8c5bd5444eea9aafac812770b723
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-12 20:51:45 +0530

    remove rol from the user auth table

--- db/migrations/000002_create_user_auth_tokens_table.sql
+++ db/migrations/000002_create_user_auth_tokens_table.sql
@@ -6,7 +6,6 @@ CREATE TABLE user_auth_token (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL,
     token VARCHAR(512) NOT NULL,
-    role VARCHAR(5) NOT NULL CHECK (role IN ('user')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
