6459ee426d0f5464fa72dc1a7c7da98280be5454
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-12 20:54:49 +0530

    add seed data for user relation using faker

--- db/migrations/000009_add_users_no_tx.go
+++ db/migrations/000009_add_users_no_tx.go
@@ -3,9 +3,11 @@ package migrations
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
+	"github.com/go-faker/faker/v4"
 	"github.com/pressly/goose/v3"
-	"github.com/raganrrlaw/server/internal/utils"
+	"github.com/raganrrlaw/server/internal/utils/hash"
 )
 
 func init() {
@@ -13,25 +15,42 @@ func init() {
 }
 
 func Up000009(ctx context.Context, db *sql.DB) error {
-	pd, err := utils.HashPassword("12345678")
+	pd, err := hash.HashPassword("12345678")
 	if err != nil {
 		return err
 	}
+
 	query := `
 		INSERT INTO users (username, first_name, last_name, email, contact_number, password_digest) 
 		VALUES ($1, $2, $3, $4, $5, $6) 
 	`
-	if _, err := db.ExecContext(
-		ctx,
-		query,
-		"sapumal_bandara",
-		"Sapumal",
-		"Bandara",
-		"sapumal_banadara@yahoo.com",
-		"0771002002",
-		pd,
-	); err != nil {
-		return err
+	for i := 0; i < 20; i++ {
+		var firstName string
+		if i%2 == 0 {
+			firstName = faker.FirstNameMale()
+		} else {
+			firstName = faker.FirstNameFemale()
+		}
+		j, err := faker.RandomInt(100, 820, 8)
+		if err != nil {
+			return err
+		}
+		var lastName string = faker.LastName()
+		var contact_number string = faker.E164PhoneNumber()
+		var username string = fmt.Sprintf("%s_%s%d", firstName, lastName, j[0])
+		var email string = fmt.Sprintf("%s_%s%d@%s", firstName, lastName, j[0], faker.DomainName())
+		if _, err := db.ExecContext(
+			ctx,
+			query,
+			username,
+			firstName,
+			lastName,
+			email,
+			contact_number,
+			pd,
+		); err != nil {
+			return err
+		}
 	}
 	return nil
 }
