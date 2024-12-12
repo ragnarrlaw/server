0eac254afa0381571c5103b1cba0c620a961ac50
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-12 21:00:06 +0530

    add a new package for hash utils

---
+++ internal/utils/hash/hash.go
@@ -0,0 +1,20 @@
+package hash
+
+/**
+add the hashing operations related utils here
+*/
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+func CheckPasswordHash(hash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
