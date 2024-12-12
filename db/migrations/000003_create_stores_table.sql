313a34d3e5efdf68215a3d33acf4ab866ea36d71
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-12 20:52:28 +0530

    update field names (remove store_ prefix) and remove the role fields from the store relation

--- db/migrations/000003_create_stores_table.sql
+++ db/migrations/000003_create_stores_table.sql
@@ -5,13 +5,13 @@ CREATE EXTENSION IF NOT EXISTS "citext";
 
 CREATE TABLE store (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    store_username VARCHAR(255) NOT NULL UNIQUE,
-    store_name VARCHAR(255) NOT NULL,
-    store_address VARCHAR(255),
-    store_email VARCHAR(255),
-    store_contact_number VARCHAR(20),
-    store_location_point GEOGRAPHY(Point, 4326),
-    store_web_url VARCHAR(255),
+    username VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    email VARCHAR(255),
+    contact_number VARCHAR(20),
+    location_point GEOGRAPHY(Point, 4326),
+    web_url VARCHAR(255),
     password_digest VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
