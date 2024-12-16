c76edcb0cb28a188b1aa1db33ccdaeeeed58b7ac
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-16 21:16:29 +0530

    add new store related data types

---
+++ internal/types/store/types.go
@@ -0,0 +1,35 @@
+package store
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/raganrrlaw/server/internal/types/geocode"
+)
+
+type Store struct {
+	Id             uuid.UUID     `json:"id"`
+	Username       string        `json:"username"`
+	Name           string        `json:"name"`
+	Address        string        `json:"address"`
+	ContactNumber  string        `json:"contactNumber"`
+	Email          string        `json:"email"`
+	WebUrl         string        `json:"webUrl"`
+	PasswordDigest string        `json:"-"`
+	Discounts      []string      `json:"discounts"`
+	LocationPoint  geocode.Point `json:"locationPoint"`
+	CreatedAt      time.Time     `json:"-"`
+	UpdatedAt      time.Time     `json:"-"`
+}
+
+type StoreStat struct {
+	Name                         string        `json:"name"`
+	Address                      string        `json:"address"`
+	Discounts                    []string      `json:"discounts"`
+	Point                        geocode.Point `json:"location"`
+	CreatedAt                    time.Time     `json:"createdAt"`
+	TotalNumberOfProducts        int           `json:"totalNumberOfProducts"`
+	NumberOfLimitedStockProducts int           `json:"numberOfLimitedStockProducts"`
+	NumberOfAvailableProducts    int           `json:"numberOfAvailableProducts"`
+	NumberOfOutOfStockProducts   int           `json:"numberOfOutOfStockProducts"`
+}
