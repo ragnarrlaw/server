945b10627015d35a3855b8913403d96bbec277c1
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-16 21:10:23 +0530

    update mapper type comments

--- internal/utils/mappers/mapper.go
+++ internal/utils/mappers/mapper.go
@@ -7,25 +7,23 @@ package mappers
 
 type MapFunc[T any, U any] func(v T) U
 
-/*
-*
-
-	Mapper a basic mapping between the domain of T and the taget domain V
+/**
+Mapper a basic mapping between the domain of T and the taget domain V
 	Example usage :=
 	var stringToInt MapFunc[string, int] = MapFunc[string, int](func (v string) int {
 	  // implement the conversion here
 	  return 0
 	})
 */
+
 func (m MapFunc[T, U]) Map(v T) U {
 	return m(v)
 }
 
-/*
-*
-
-	For mapping a collection of values from the domain of T the domain of U
+/**
+For mapping a collection of values from the domain of T the domain of U
 */
+
 func (m MapFunc[T, U]) MapEach(v []T) []U {
 	results := make([]U, len(v))
 	for index, item := range v {
@@ -34,12 +32,11 @@ func (m MapFunc[T, U]) MapEach(v []T) []U {
 	return results
 }
 
-/*
-*
-
-	For mapping with cases where the step prior to the conversion could fail
+/**
+For mapping with cases where the step prior to the conversion could fail
 	convert only if an error didn't occur - it returns a base value
 */
+
 func (m MapFunc[T, U]) MapErr(v T, err error) U {
 	if err != nil {
 		var zero U
@@ -48,11 +45,10 @@ func (m MapFunc[T, U]) MapErr(v T, err error) U {
 	return m(v)
 }
 
-/*
-*
-
-	For mapping a collection of values from the domain of T the domain of U
+/**
+For mapping a collection of values from the domain of T the domain of U
 */
+
 func (m MapFunc[T, U]) MapEachErr(v []T, err error) []U {
 	if err != nil {
 		return make([]U, 0)
