839020eaf80f10869f187d4b7656189b7ea985f7
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-16 21:24:32 +0530

    update product handlers according to new repository functions, remove old handlers, add new response data type support for handlers

--- internal/services/product_service/product_service.go
+++ internal/services/product_service/product_service.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	productrepository "github.com/raganrrlaw/server/internal/services/product_service/product_repository"
-	"github.com/raganrrlaw/server/internal/types"
+	"github.com/raganrrlaw/server/internal/types/search"
 )
 
 type ProductService struct {
@@ -20,46 +20,40 @@ func NewProductService(productRepository productrepository.ProductRepository) *P
 	}
 }
 
-func (ps *ProductService) AddProductHandler(w http.ResponseWriter, r *http.Request) {
-	payload := &types.ProductPayload{}
-	err := json.NewDecoder(r.Body).Decode(payload)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
+func (ps *ProductService) GetProductsHandler(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
 	defer cancel()
 
-	/** TODO: PAYLOAD VALIDATION */
-	/** TODO: GENERIC RESPONSE ERROR STRUCTURE ORGANIZATION */
-
-	if storeId := r.PathValue("storeId"); storeId != "" {
-		if product, err := ps.productRepository.Add(ctx, storeId, payload); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+	if products, err := ps.productRepository.Get(ctx); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		if totalProducts, err := ps.productRepository.GetTotalNumberOfProducts(ctx); err != nil {
 		} else {
-			if b, err := json.Marshal(product); err != nil {
+			if b, err := json.Marshal(search.Payload{
+				Data: products,
+				Meta: search.Meta{
+					Total: totalProducts,
+				},
+			}); err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 			} else {
 				w.Header().Set("Content-Type", "application/json")
-				w.WriteHeader(http.StatusCreated)
+				w.WriteHeader(http.StatusOK)
 				w.Write(b)
 			}
 		}
-	} else {
-		http.Error(w, "Invalid store id", http.StatusBadRequest)
 	}
 }
 
-func (ps *ProductService) GetAllProductsHandler(w http.ResponseWriter, r *http.Request) {
+func (ps *ProductService) GetProductHandler(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
 	defer cancel()
 
-	if storeId := r.PathValue("storeId"); storeId != "" {
-		if products, err := ps.productRepository.GetAll(ctx, storeId); err != nil {
+	if productId := r.PathValue("productId"); productId != "" {
+		if product, err := ps.productRepository.GetById(ctx, productId); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		} else {
-			if b, err := json.Marshal(products); err != nil {
+			if b, err := json.Marshal(product); err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 			} else {
 				w.Header().Set("Content-Type", "application/json")
@@ -68,261 +62,32 @@ func (ps *ProductService) GetAllProductsHandler(w http.ResponseWriter, r *http.R
 			}
 		}
 	} else {
-		http.Error(w, "Invalid store id", http.StatusBadRequest)
+		http.Error(w, "Invalid product id", http.StatusBadRequest)
 	}
 }
 
-func (ps *ProductService) GetProductHandler(w http.ResponseWriter, r *http.Request) {
+func (ps *ProductService) GetSearchProductByNameHandler(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
 	defer cancel()
 
-	if storeId := r.PathValue("storeId"); storeId != "" {
-		if productId := r.PathValue("productId"); productId != "" {
-			if product, err := ps.productRepository.GetById(ctx, storeId, productId); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			} else {
-				if b, err := json.Marshal(product); err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-				} else {
-					w.Header().Set("Content-Type", "application/json")
-					w.WriteHeader(http.StatusOK)
-					w.Write(b)
-				}
-			}
-		} else {
-			http.Error(w, "Invalid product id", http.StatusBadRequest)
-		}
-	} else {
-		http.Error(w, "Invalid store id", http.StatusBadRequest)
-	}
-}
-
-func (ps *ProductService) UpdateProductHandler(w http.ResponseWriter, r *http.Request) {
-	payload := &types.ProductPayload{}
-	err := json.NewDecoder(r.Body).Decode(payload)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
-	defer cancel()
-
-	if storeId := r.PathValue("storeId"); storeId != "" {
-		if productId := r.PathValue("productId"); productId != "" {
-			if product, err := ps.productRepository.Update(ctx, storeId, productId, payload); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			} else {
-				if b, err := json.Marshal(product); err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-				} else {
-					w.Header().Set("Content-Type", "application/json")
-					w.WriteHeader(http.StatusOK)
-					w.Write(b)
-				}
-			}
-		} else {
-			http.Error(w, "Invalid product id", http.StatusBadRequest)
-		}
-	} else {
-		http.Error(w, "Invalid store id", http.StatusBadRequest)
-	}
-}
-
-func (ps *ProductService) RemoveProductHandler(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
-	defer cancel()
-
-	if storeId := r.PathValue("storeId"); storeId != "" {
-		if productId := r.PathValue("productId"); productId != "" {
-			if err := ps.productRepository.Remove(
-				ctx,
-				storeId,
-				[]string{productId},
-			); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			} else {
-				w.WriteHeader(http.StatusNoContent)
-			}
-		} else {
-			http.Error(w, "Invalid product id", http.StatusBadRequest)
-		}
-	} else {
-		http.Error(w, "Invalid store id", http.StatusBadRequest)
-	}
-}
-
-// product discount handlers
-func (ps *ProductService) GetProductDiscountsHandler(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
-	defer cancel()
-
-	if storeId := r.PathValue("storeId"); storeId != "" {
-		if productId := r.PathValue("productId"); productId != "" {
-			if discounts, err := ps.productRepository.GetProductDiscounts(
-				ctx,
-				storeId,
-				productId,
-			); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			} else {
-				if b, err := json.Marshal(discounts); err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-				} else {
-					w.Header().Set("Content-Type", "application/json")
-					w.WriteHeader(http.StatusOK)
-					w.Write(b)
-				}
-			}
-		} else {
-			http.Error(w, "Invalid product id", http.StatusBadRequest)
-		}
-	} else {
-		http.Error(w, "Invalid store id", http.StatusBadRequest)
-	}
-}
-
-func (ps *ProductService) AddProductDiscountHandler(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
-	defer cancel()
-	payload := &types.DiscountPayload{}
-
-	if err := json.NewDecoder(r.Body).Decode(payload); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	} else {
-		if storeId := r.PathValue("storeId"); storeId != "" {
-			if productId := r.PathValue("productId"); productId != "" {
-				if discount, err := ps.productRepository.AddProductDiscount(
-					ctx,
-					storeId,
-					productId,
-					payload,
-				); err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-				} else {
-					if b, err := json.Marshal(discount); err != nil {
-						http.Error(w, err.Error(), http.StatusInternalServerError)
-					} else {
-						w.Header().Set("Content-Type", "application/json")
-						w.WriteHeader(http.StatusCreated)
-						w.Write(b)
-					}
-				}
-			} else {
-				http.Error(w, "Invalid product id", http.StatusBadRequest)
-			}
-		} else {
-			http.Error(w, "Invalid store id", http.StatusBadRequest)
-		}
-	}
-}
-
-func (ps *ProductService) UpdateProductDiscountHandler(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
-	defer cancel()
-	payload := &types.DiscountPayload{}
-
-	if err := json.NewDecoder(r.Body).Decode(payload); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	if storeId := r.PathValue("storeId"); storeId != "" {
-		if productId := r.PathValue("productId"); productId != "" {
-			if discountId := r.PathValue("discountId"); discountId != "" {
-				if discount, err := ps.productRepository.UpdateDiscount(
-					ctx,
-					storeId,
-					productId,
-					discountId,
-					payload,
-				); err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-				} else {
-					if b, err := json.Marshal(discount); err != nil {
-						http.Error(w, err.Error(), http.StatusInternalServerError)
-					} else {
-						w.Header().Set("Content-Type", "application/json")
-						w.WriteHeader(http.StatusOK)
-						w.Write(b)
-					}
-				}
-			} else {
-				http.Error(w, "Invalid product id", http.StatusBadRequest)
-			}
-		} else {
-			http.Error(w, "Invalid product id", http.StatusBadRequest)
-		}
-	} else {
-		http.Error(w, "Invalid store id", http.StatusBadRequest)
-	}
-
-}
-
-func (ps *ProductService) RemoveProductDiscountHandler(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
-	defer cancel()
-
-	if storeId := r.PathValue("storeId"); storeId != "" {
-		if productId := r.PathValue("productId"); productId != "" {
-			if discountId := r.PathValue("discountId"); discountId != "" {
-				if err := ps.productRepository.RemoveDiscount(
-					ctx,
-					storeId,
-					productId,
-					[]string{discountId},
-				); err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-				} else {
-					w.WriteHeader(http.StatusNoContent)
-				}
-			} else {
-				http.Error(w, "Invalid discount id", http.StatusBadRequest)
-			}
-		} else {
-			http.Error(w, "Invalid product id", http.StatusBadRequest)
-		}
-	} else {
-		http.Error(w, "Invalid store id", http.StatusBadRequest)
-	}
-}
-
-func (ps *ProductService) RemoveAllProductDiscountHandler(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
-	defer cancel()
-
-	if storeId := r.PathValue("storeId"); storeId != "" {
-		if productId := r.PathValue("productId"); productId != "" {
-			if err := ps.productRepository.RemoveAllDiscounts(
-				ctx,
-				storeId,
-				productId,
-			); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			} else {
-				w.WriteHeader(http.StatusNoContent)
-			}
-		} else {
-			http.Error(w, "Invalid product id", http.StatusBadRequest)
-		}
-	} else {
-		http.Error(w, "Invalid store id", http.StatusBadRequest)
-	}
-}
-
-// product categories
-func (ps *ProductService) GetAllProductCategoriesHandler(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
-	defer cancel()
-
-	if categories, err := ps.productRepository.ProductCategories(ctx); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	} else {
-		if b, err := json.Marshal(categories); err != nil {
+	if term := r.URL.Query().Get("term"); term != "" {
+		if products, err := ps.productRepository.SearchProductByName(ctx, term); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		} else {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			w.Write(b)
+			if b, err := json.Marshal(search.Payload{
+				Data: products,
+				Meta: search.Meta{
+					Total: uint(len(*products)),
+				},
+			}); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			} else {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				w.Write(b)
+			}
 		}
+	} else {
+		http.Error(w, "Invalid product id", http.StatusBadRequest)
 	}
 }
