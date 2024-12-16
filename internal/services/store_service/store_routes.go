b5ecae32b254f2c1643793776a774feffd0cb93f
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-16 21:20:36 +0530

    remove dead routes for unused store handlers

--- internal/services/store_service/store_routes.go
+++ internal/services/store_service/store_routes.go
@@ -6,8 +6,5 @@ func (ss *StoreService) RegisterRoutes(router *http.ServeMux) {
 	// store routes
 	router.HandleFunc("GET /store", ss.GetAllStoresHandler)
 	router.HandleFunc("GET /store/{id}", ss.GetStoreHandler)
-	router.HandleFunc("POST /store", ss.AddStoreHandler)
-	router.HandleFunc("PATCH /store/{id}", ss.UpdateStoreHandler)
-	router.HandleFunc("PATCH /store/{id}/location", ss.UpdateStoreLocationHandler)
-	router.HandleFunc("DELETE /store/{id}", ss.RemoveStoreHandler)
+	router.HandleFunc("GET /store/stat", ss.GetStoreStatHandler)
 }
