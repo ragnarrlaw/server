35e243a9f31b2e8620d41e957917090284fb5638
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-16 21:21:48 +0530

    update recommender service and add support for new response payload structure

--- internal/services/recommender_service/recommender_service.go
+++ internal/services/recommender_service/recommender_service.go
@@ -1,3 +1,69 @@
 package recommenderservice
 
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
 
+	recommenderrepository "github.com/raganrrlaw/server/internal/services/recommender_service/recommender_repository"
+	"github.com/raganrrlaw/server/internal/types/recommender"
+	"github.com/raganrrlaw/server/internal/types/search"
+)
+
+type RecommenderService struct {
+	recommenderRepository recommenderrepository.RecommenderRepository
+}
+
+func NewRecommenderService(recommenderRepo recommenderrepository.RecommenderRepository) *RecommenderService {
+	return &RecommenderService{
+		recommenderRepository: recommenderRepo,
+	}
+}
+
+func (rs *RecommenderService) SearchHandler(w http.ResponseWriter, r *http.Request) {
+	payload := &recommender.Search{}
+
+	if err := json.NewDecoder(r.Body).Decode(payload); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	} else {
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+		if recommendations, err := rs.recommenderRepository.Search(ctx, payload); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		} else {
+			if b, err := json.Marshal(search.Payload{
+				Data: recommendations,
+			}); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			} else {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusCreated)
+				w.Write(b)
+			}
+		}
+	}
+}
+
+func (rs *RecommenderService) LineStringSearchHandler(w http.ResponseWriter, r *http.Request) {
+	payload := &recommender.LineStringSearch{}
+	if err := json.NewDecoder(r.Body).Decode(payload); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	} else {
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+		if recommendations, err := rs.recommenderRepository.LineStringSearch(ctx, payload); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		} else {
+			if b, err := json.Marshal(search.Payload{
+				Data: recommendations,
+			}); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			} else {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusCreated)
+				w.Write(b)
+			}
+		}
+	}
+}
