711e6ea1a5fa81d225a9c9b3ceee2d056de7d362
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-16 21:24:51 +0530

    remove old routes, update new product routes

--- internal/services/product_service/product_routes.go
+++ internal/services/product_service/product_routes.go
@@ -4,19 +4,7 @@ import "net/http"
 
 func (ps *ProductService) RegisterRoutes(router *http.ServeMux) {
 	// Product routes
-	router.HandleFunc("GET /store/{storeId}/product", ps.GetAllProductsHandler)
-	router.HandleFunc("GET /store/{storeId}/product/{productId}", ps.GetProductHandler)
-	router.HandleFunc("POST /store/{storeId}/product", ps.AddProductHandler)
-	router.HandleFunc("PATCH /store/{storeId}/product/{productId}", ps.UpdateProductHandler)
-	router.HandleFunc("DELETE /store/{storeId}/product/{productId}", ps.RemoveProductHandler)
-
-	// Discount routes
-	router.HandleFunc("GET /store/{storeId}/product/{productId}/discount", ps.GetProductDiscountsHandler)
-	router.HandleFunc("POST /store/{storeId}/product/{productId}/discount", ps.AddProductDiscountHandler)
-	router.HandleFunc("PATCH /store/{storeId}/product/{productId}/discount/{discountId}", ps.UpdateProductDiscountHandler)
-	router.HandleFunc("DELETE /store/{storeId}/product/{productId}/discount/{discountId}", ps.RemoveProductDiscountHandler)
-	router.HandleFunc("DELETE /store/{storeId}/product/{productId}/discount", ps.RemoveAllProductDiscountHandler)
-
-	// Category routes
-	router.HandleFunc("GET /product/category", ps.GetAllProductCategoriesHandler)
+	router.HandleFunc("GET /product", ps.GetProductsHandler)
+	router.HandleFunc("GET /product/{productId}", ps.GetProductHandler)
+	router.HandleFunc("GET /product/search", ps.GetSearchProductByNameHandler)
 }
