639facb6033986a02169e5fbcc373b7431a6b344
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-16 21:05:38 +0530

    update product table data types and field names to fit schemas and create the recommendation_item_t for fast scanning (postgres custom composite types)

--- db/migrations/000005_create_products_table.sql
+++ db/migrations/000005_create_products_table.sql
@@ -14,16 +14,16 @@ CREATE TABLE category (
 
 CREATE TABLE product (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    code VARCHAR(255),
+    code VARCHAR(255) DEFAULT '',
     name VARCHAR(1000) NOT NULL,
-    description TEXT,
-    brand VARCHAR(1000),
-    brand_tags TEXT,
+    description TEXT DEFAULT '',
+    brand VARCHAR(1000) DEFAULT '',
+    brand_tags TEXT DEFAULT '',
     category_id UUID DEFAULT NULL,
-    labels TEXT,
-    image_url TEXT,
-    product_quantity VARCHAR(255),
-    serving_size VARCHAR(255),
+    labels TEXT DEFAULT '',
+    image_url TEXT DEFAULT '',
+    product_quantity VARCHAR(255) DEFAULT '',
+    serving_size VARCHAR(255) DEFAULT '',
     unit_of_measure VARCHAR(20) NOT NULL CHECK (unit_of_measure IN ('pcs', 'g', 'kg', 'ml', 'l')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -59,7 +59,18 @@ CREATE TABLE store_product (
     UNIQUE(store_id, product_id)
 );
 
+CREATE TYPE recommendation_item_t AS (
+    product_id UUID,
+    price_per_unit NUMERIC,
+    listed_uom TEXT,
+    stock_quantity INT,
+    product_name TEXT,
+    product_image_url TEXT,
+    standard_uom TEXT
+);
+
 -- +goose Down
 DROP TABLE store_product;
 DROP TABLE product;
+DROP TYPE IF EXISTS recommendation_item_t;
 DROP TABLE category;
