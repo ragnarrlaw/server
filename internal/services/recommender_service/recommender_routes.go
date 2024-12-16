b205f0c7d55f5a108d87f5af3cd79d9e23a01978
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-16 21:22:01 +0530

    update recommender routes

--- internal/services/recommender_service/recommender_routes.go
+++ internal/services/recommender_service/recommender_routes.go
@@ -1,2 +1,8 @@
 package recommenderservice
 
+import "net/http"
+
+func (rs *RecommenderService) RegisterRoutes(router *http.ServeMux) {
+	router.HandleFunc("POST /recommender/search", rs.SearchHandler)
+	router.HandleFunc("POST /recommender/search/line-string", rs.LineStringSearchHandler)
+}
