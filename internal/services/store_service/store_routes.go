e65e5a28990e871053263347580fc73a813ce0ed
ragnarrlaw <carmichealclair@yahoo.com>
2024-12-24 21:04:59 +0530

    add new handlers and routes

--- internal/services/store_service/store_routes.go
+++ internal/services/store_service/store_routes.go
@@ -5,6 +5,19 @@ import "net/http"
 func (ss *StoreService) RegisterRoutes(router *http.ServeMux) {
 	// store routes
 	router.HandleFunc("GET /store", ss.GetAllStoresHandler)
-	router.HandleFunc("GET /store/{id}", ss.GetStoreHandler)
+	router.HandleFunc("GET /store/{storeId}", ss.GetStoreHandler)
 	router.HandleFunc("GET /store/stat", ss.GetStoreStatHandler)
+	router.HandleFunc("POST /store/search", ss.PostStoreSearchHandler)
+
+	// store product routes
+	router.HandleFunc("GET /store/{storeId}/product", ss.GetStoreProductsHandler)
+	router.HandleFunc("POST /store/{storeId}/product", ss.PostStoreProductHandler)
+	router.HandleFunc("PATCH /store/{storeId}/product/{productId}", ss.UpdateStoreProductHandler)
+	router.HandleFunc("DELETE /store/{storeId}/product", ss.DeleteStoreProductsHandler)
+
+	// store discount routes
+	router.HandleFunc("GET /store/{storeId}/discount", ss.GetStoreDiscountHandler)
+	router.HandleFunc("POST /store/{storeId}/discount", ss.PostStoreDiscountHandler)
+	router.HandleFunc("PATCH /store/{storeId}/discount/{discountId}", ss.UpdateStoreDiscountHandler)
+	router.HandleFunc("DELETE /store/{storeId}/discount", ss.DeleteStoreDiscountHandler)
 }
